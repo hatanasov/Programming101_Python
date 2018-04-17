e75a628b923272b1d7590d1aed41edfb649d4a72
hatanasov <h.atanasov87@gmail.com>
2018-04-17 18:06:46 +0300

    final decorators

--- Week6/Decorators/decorators.py
+++ Week6/Decorators/decorators.py
@@ -31,27 +31,77 @@ def accepts(*args):
 # print(deposit("ico", {"age": 10}))
 
 
-
 def encrypt(key):
     def encrypter(func):
+        @wraps(func)
         def decorated():
             message = func()
             alphabet_lower = string.ascii_lowercase
             alphabet_upper = string.ascii_uppercase
-            shifted_alphabet_lower = alphabet_lower[key:] + alphabet_lower[:key]
-            shifted_alphabet_upper = alphabet_upper[key:] + alphabet_upper[:key]
+            shifted_alphabet_lower = alphabet_lower[key:] + \
+                alphabet_lower[:key]
+            shifted_alphabet_upper = alphabet_upper[key:] + \
+                alphabet_upper[:key]
             lower_table = str.maketrans(alphabet_lower, shifted_alphabet_lower)
             upper_table = str.maketrans(alphabet_upper, shifted_alphabet_upper)
             result_str = message.translate(lower_table).translate(upper_table)
-            return result_str
+            print(result_str)
+            return func
         return decorated
     return encrypter
 
 
-@encrypt(2)
-def get_low():
-    return "Get get get low"
+def log(log_file):
+    def logger(origin_func):
+        @wraps(origin_func)
+        def decorated():
+            import logging
+            from datetime import datetime
+            now = str(datetime.now())
+            origin_func_name = origin_func.__name__
+            with open(log_file, "a") as f:
+                log = "{} was called ad {}\n".format(origin_func_name, now)
+                f.write(log)
+            return origin_func()
+        return decorated
+    return logger
 
 
-print(get_low())
+# @log("logs.txt")
+# @encrypt(2)
+# def get_low():
+#     return "Get get get low"
 
+
+# get_low()
+def performance(log_file):
+    def logger(origin_func):
+        @wraps(origin_func)
+        def decorated_logger():
+            import datetime
+            t1 = datetime.datetime.now()
+            origin_func()
+            t2 = datetime.datetime.now()
+            time = t2 - t1
+            with open(log_file, "a") as f:
+                log = "{} was called and took {} seconds to complete\n".format(
+                    origin_func.__name__, time.total_seconds())
+                f.write(log)
+        return decorated_logger
+    return logger
+
+
+# @performance('logs.txt')
+# def something_heavy():
+#     from time import sleep
+#     sleep(2)
+#     return "I am done!"
+
+
+# @performance('logs.txt')
+# def get_low():
+#     return "Get get get low"
+
+
+# something_heavy()
+# get_low()
